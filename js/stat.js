890d9a60f52e0aa972cc1bf12b6538af554b2ccc
nanasanichi <haraokiru@gmail.com>
2019-10-03 21:49:19 +0300

    добавила функции отрисовки текста и столбиков

--- js/stat.js
+++ js/stat.js
@@ -20,33 +20,39 @@ var getMaxElement = function (arr) {
 };
 
 var getRandomNumber = function (min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Функция отрисовки текста
+var renderText = function (ctx, fontSize, text, x, y) {
+  ctx.font = fontSize + 'px PT Mono';
+  ctx.fillStyle = '#000000';
+  ctx.fillText(text, x, y);
+};
+
+// Функция отрисовки столбика
+var renderBar = function (ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = '#000';
-
-  ctx.font = '16px PT Mono';
-  // ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP_X, CLOUD_Y + GAP + TEXT_HEIGHT);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP_X, CLOUD_Y + GAP * 1.5 + TEXT_HEIGHT * 2);
+  renderText(ctx, 16, 'Ура вы победили!', CLOUD_X + GAP_X, CLOUD_Y + GAP + TEXT_HEIGHT);
+  renderText(ctx, 16, 'Список результатов:', CLOUD_X + GAP_X, CLOUD_Y + GAP * 1.5 + TEXT_HEIGHT * 2);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - TEXT_HEIGHT - (times[i] * BAR_HEIGTH / maxTime));
+    renderText(ctx, 16, names[i], CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
+    renderText(ctx, 16, Math.floor(times[i]), CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - TEXT_HEIGHT - (times[i] * BAR_HEIGTH / maxTime));
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'hsl(240, ' + getRandomNumber(1, 100) + '%, ' + getRandomNumber(50, 100) + '%)';
+      ctx.fillStyle = 'hsl(240, ' + getRandomNumber(1, 100) + '%, ' + '50%)';
     }
-    ctx.fillRect(CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT, BAR_WIDTH, GAP + TEXT_HEIGHT - (times[i] * BAR_HEIGTH / maxTime));
+    renderBar(ctx, CLOUD_X + GAP_X + (GAP_X + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT, BAR_WIDTH, GAP + TEXT_HEIGHT - (times[i] * BAR_HEIGTH / maxTime));
   }
 };
-
